437a419372f09fb1f4c0163193954924a802b6d1
vinfork <82943416+vinfork@users.noreply.github.com>
2021-11-16 13:26:27 +0100

    Update integrated_training_process.m

--- integrated_training_process.m
+++ integrated_training_process.m
@@ -13,14 +13,56 @@ disp('Loading training data...')
 % Y: output / for Classification: label
 % for example: XTrain, YTrain, XTest, YTest, XValid, YValid
 
-%% define network
-%   most basic network
-NN_layers = [ ...
+%httpsUrl = 'http://yann.lecun.com/exdb/mnist';
+%imageUrl = strcat(httpsUrl, '/train-images-idx3-ubyte.gz');
+%XTrain = webread(imageUrl);
 
-];
+digitDatasetPath = fullfile(matlabroot,'toolbox','nnet','nndemos', ...
+    'nndatasets','DigitDataset');
+fullDataset = imageDatastore(digitDatasetPath, ...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
 
-% visualize the neural network
-analyzeNetwork(NN_layers)
+% XTrain = fullDataset.Files{:};   %Image
+% YTrain = fullDataset.Labels{:};   %Label
+% XTest = fullDataset.Files{:};    %Image
+% YTest = fullDataset.Labels{:};    %Label
+% XValid =fullDataset.Files{:};    %Image
+% YValid = fullDataset.Labels{:};    %Label
+
+figure;
+perm = randperm(10000,20);
+for i = 1:20
+    subplot(4,5,i);
+    imshow(imds.Files{perm(i)});
+end
+labelCount = countEachLabel(imds)
+
+img = readimage(imds,1);
+size(img)
+
+numTrainFiles = 750;
+[TrainSet,ValidationSet] = splitEachLabel(fullDataset,numTrainFiles,'randomize');
+%% define network (dlnet)
+NN_layers = [
+    imageInputLayer([28 28 1],'Normalization','none','Name','input')
+    
+    fullyConnectedLayer(1000, 'Name','fullyConnected1')
+    reluLayer('Name','relu1')
+    
+    fullyConnectedLayer(10,'Name','fullyConnected2')
+    softmaxLayer('Name','softmax')
+    
+    classificationLayer('Name','classification')
+   ];
+
+% % convert to a layer graph
+% lgraph = layerGraph(NN_layers);
+% % Create a dlnetwork object from the layer graph.
+% 
+% dlnet = dlnetwork(lgraph);
+% % visualize the neural network
+% analyzeNetwork(dlnet)
+
 %% Specify Training Options (define hyperparameters)
 
 % miniBatchSize
@@ -28,7 +70,6 @@ analyzeNetwork(NN_layers)
 % learnRate 
 % executionEnvironment
 % numIterationsPerEpoch 
-% solver "sgdm" "rmsprop" "adam"
 
 % training on CPU or GPU(if available);
 % 'auto': Use a GPU if one is available. Otherwise, use the CPU.
@@ -37,13 +78,86 @@ analyzeNetwork(NN_layers)
 % 'multi-gpu' :Use multiple GPUs
 % 'parallel :
 
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate',0.001, ...
+    'MaxEpochs',5, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',ValidationSet, ...
+    'ValidationFrequency',30, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+net = trainNetwork(TrainSet,NN_layers,options);
 
-%%  Train neural network
-% define "trainingOptions"
-% training using "trainNetwork"
+[net, lr_001] =  trainNetwork(TrainSet,NN_layers,options);
 
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate',0.0001, ...
+    'MaxEpochs',5, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',ValidationSet, ...
+    'ValidationFrequency',30, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+net = trainNetwork(TrainSet,NN_layers,options);
+
+[net, lr_0001] =  trainNetwork(TrainSet,NN_layers,options);
+
+%% 
+disp(lr_001)
+disp(lr_0001)
+x = reshape(lr_001.ValidationLoss(:, 1:289), [], 1)
+y = reshape(lr_0001.ValidationLoss(:, 1:289), [], 1)
+
+x(find(isnan(x)))=[]
+y(find(isnan(y)))=[]
+
+plot(x)
+hold on
+plot(y)
+
+legend('lr 0,001','lr 0,0001')
+%% Train neural network
+
+% initialize the average gradients and squared average gradients
+% averageGrad
+% averageSqGrad
+
+% "for-loop " for training
+
+for epoch = 1:numEpochs
+    
+   % updae learnable parameters based on mini-batch of data
+    for i = 1:numIterationsPerEpoch
+        % Read mini-batch of data and convert the labels to dummy variables.
+
+
+        % Convert mini-batch of data to a dlarray.
+        
+        
+        % Evaluate the model gradients and loss using dlfeval and the
+        % modelGradients helper function.
+        
+        % Update the network parameters using the optimizer, like SGD, Adam
+        
+        % Calculate accuracy & show the training progress.
+
+        % option: validation
+
+    end
+end
 
 
 %% test neural network & visualization 
-% Calculate accuracy
 
+%% Define Model Gradients Function
+% 
+function [gradients,loss,dlYPred] = modelGradients(dlnet,dlX,Y)
+
+    % forward propagation 
+    dlYPred = forward(dlnet,dlX);
+    % calculate loss -- varies based on different requirement
+    loss = crossentropy(dlYPred,Y);
+    % calculate gradients 
+    gradients = dlgradient(loss,dlnet.Learnables);
+    
+end
