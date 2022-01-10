7ae4ca17a57b5acf35048dcb06e37dffda8bf56f
JulianSchmidt96 <69909913+JulianSchmidt96@users.noreply.github.com>
2022-01-10 15:51:58 +0100

    finished

--- versuch3/NNBV_Versuch3_MATLAB/upload_for_students/dataset_generation.m
+++ versuch3/NNBV_Versuch3_MATLAB/upload_for_students/dataset_generation.m
@@ -8,18 +8,38 @@ image_size = 32;    % resolution 32x32
  
 %% generation of complex mode weights and label vector - Aufgabe 1
 
+
 % 1. create random amplitude weights. The weights of amplitude should be normalized.
+rho=rand(1,number_of_data);
+rho_n=rho/norm(rho);
+
 
 % 2. create random phase amplitude. (Using realtive phase difference)
+phase = rand(number_of_data,1);
+phase = phase/norm(phase); %normalize
+
+phase = phase - phase(1); % make phase relative to first mode (phase difference)
+
 
 % 3. complex mode weights vector
 
+weights_vector = rho_n.*exp(1i*phase);
+
 % 4. normalize cos(phase) to (0,1)
 
-% 5. combine amplitude and phase into a label vector (1,2N-1)
+cos_n = normalization(cos(phase),0,1);
 
-% 6. split complex mode weights vector and label vector into Training,
-% validation and test set. 
+% 5. combine amplitude and phase into a label vector (1,2N-1)
+label_vector = rho_n.*exp(1i*cos_n);
+
+% 6. split complex mode weights vector and label vector into Training, validation and test set. 
+trainset_weights = weights_vector(1:floor(number_of_data/3),:);
+validationset_weights = weights_vector(floor(number_of_data/3)+1:floor(2*number_of_data/3),:);
+testset_weights = weights_vector(floor(2*number_of_data/3)+1:end,:);
+
+trainset_labels = label_vector(1:floor(number_of_data/3),:);
+validationset_labels = label_vector(floor(number_of_data/3)+1:floor(2*number_of_data/3),:);
+testset_labels = label_vector(floor(2*number_of_data/3)+1:end,:);
 
 %% create image data - Aufgabe 2 
 % use function mmf_build_image()
